4a7f5cde2cd047f11b4338bac7f7d9344528a01d
SpacecOOk <46821609+SpacecOOk@users.noreply.github.com>
2019-01-22 11:07:18 -0600

    Michel Antoine Dionne Gutierrez Grupo 03

---
+++ Velocidadpromedio.py
@@ -0,0 +1,6 @@
+#Autor: Michel Antojne Dionne Gutierrez A01748632
+#Calcula la velocidad promedio, dado el tiempo y la distancia
+t=float(input("Dame el tiempo en segundos: "))
+d=float(input("Dame la distancia en metros: "))
+v=d/t
+print("La velocidad promedio es de",v,"m/s")
